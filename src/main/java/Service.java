990bbdfccb5ca8fb7fd1b1f54e3d8144b7f56769
kalle <kalle@kodapan.se>
2015-09-21 19:18:07 +0200

    felhantering vid dåliga urlar

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -13,6 +13,10 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.URLEncoder;
 import java.util.*;
 
@@ -24,79 +28,100 @@ public class Service {
 
   public static void main(String[] args) throws Exception {
 
-    XPathFactory xPathfactory = XPathFactory.newInstance();
-    XPath xpath = xPathfactory.newXPath();
-    XPathExpression linksExpression = xpath.compile("//DIV[@class='mw-spcontent']/OL/LI");
-    XPathExpression externalLinkExpression = xpath.compile("A[1]/@href");
-    XPathExpression wikipediaLinkExpression = xpath.compile("A[2]/@href");
-
-
-    Map<String, Set<String>> pagesByURL = new HashMap<>();
-
-    String head = "https://sv.wikipedia.org/w/index.php?title=Special:L%C3%A4nks%C3%B6kning";
-
-    int limit = 10000;
-
-    String query;
-    if (args.length == 0) {
-      query = "http://www.helsingborg.se/";
-    } else {
-      query = args[0];
-    }
-
-    CloseableHttpClient client = HttpClientBuilder.create().build();
+    Writer out = new OutputStreamWriter(new FileOutputStream(new File("out.txt")));
     try {
 
-      CloseableHttpResponse response = client.execute(new HttpGet(head + "&limit=" + limit + "&target=" + URLEncoder.encode(query, "UTF8")));
-      try {
+      XPathFactory xPathfactory = XPathFactory.newInstance();
+      XPath xpath = xPathfactory.newXPath();
+      XPathExpression linksExpression = xpath.compile("//DIV[@class='mw-spcontent']/OL/LI");
+      XPathExpression externalLinkExpression = xpath.compile("A[1]/@href");
+      XPathExpression wikipediaLinkExpression = xpath.compile("A[2]/@href");
 
-        DOMParser parser = new DOMParser();
-        parser.parse(new InputSource(response.getEntity().getContent()));
-        Document document = parser.getDocument();
 
-        NodeList links = (NodeList) linksExpression.evaluate(document, XPathConstants.NODESET);
-        for (int i = 0; i < links.getLength(); i++) {
-          Node linkNode = links.item(i);
-          String externalLink = externalLinkExpression.evaluate(linkNode);
-          String wikipediaLink = wikipediaLinkExpression.evaluate(linkNode);
+      Map<String, Set<String>> pagesByURL = new HashMap<>();
 
-          Set<String> pages = pagesByURL.get(externalLink);
-          if (pages == null) {
-            pages = new HashSet<>();
-            pagesByURL.put(externalLink, pages);
-          }
+      String head = "https://sv.wikipedia.org/w/index.php?title=Special:L%C3%A4nks%C3%B6kning";
 
-          pages.add(wikipediaLink);
+      int limit = 10000;
 
-          System.currentTimeMillis();
-        }
-
-      } finally {
-        response.close();
+      String query;
+      if (args.length == 0) {
+        query = "http://www.regeringen.se/";
+      } else {
+        query = args[0];
       }
 
-      for (String externalLink : pagesByURL.keySet()) {
+      CloseableHttpClient client = HttpClientBuilder.create().build();
+      try {
 
-        response = client.execute(new HttpGet(externalLink));
+        CloseableHttpResponse response = client.execute(new HttpGet(head + "&limit=" + limit + "&target=" + URLEncoder.encode(query, "UTF8")));
         try {
-          if (response.getStatusLine().getStatusCode() != 200) {
-            System.out.println("HTTP " + response.getStatusLine().getStatusCode() + " for " + externalLink);
-            for (String wikipediaLink : pagesByURL.get(externalLink)) {
-              System.out.println("https://sv.wikipedia.org" + wikipediaLink);
+
+          DOMParser parser = new DOMParser();
+          parser.parse(new InputSource(response.getEntity().getContent()));
+          Document document = parser.getDocument();
+
+          NodeList links = (NodeList) linksExpression.evaluate(document, XPathConstants.NODESET);
+          for (int i = 0; i < links.getLength(); i++) {
+            Node linkNode = links.item(i);
+            String externalLink = externalLinkExpression.evaluate(linkNode);
+            String wikipediaLink = wikipediaLinkExpression.evaluate(linkNode);
+
+            Set<String> pages = pagesByURL.get(externalLink);
+            if (pages == null) {
+              pages = new HashSet<>();
+              pagesByURL.put(externalLink, pages);
             }
-            System.out.println();
+
+            pages.add(wikipediaLink);
+
+            System.currentTimeMillis();
           }
+
         } finally {
           response.close();
         }
 
+        for (String externalLink : pagesByURL.keySet()) {
+
+          System.out.println(externalLink);
+
+          try {
+            response = client.execute(new HttpGet(externalLink));
+          } catch (Exception e) {
+            out.write("Bad external URL? " + externalLink + "\n");
+            out.write("HTTP " + response.getStatusLine().getStatusCode() + " for " + externalLink + "\n");
+            for (String wikipediaLink : pagesByURL.get(externalLink)) {
+              out.write("https://sv.wikipedia.org" + wikipediaLink + "\n");
+            }
+            out.write("\n");
+            out.flush();
+            continue;
+          }
+
+          try {
+            if (response.getStatusLine().getStatusCode() != 200) {
+              out.write("HTTP " + response.getStatusLine().getStatusCode() + " for " + externalLink + "\n");
+              for (String wikipediaLink : pagesByURL.get(externalLink)) {
+                out.write("https://sv.wikipedia.org" + wikipediaLink + "\n");
+              }
+              out.write("\n");
+              out.flush();
+            }
+          } finally {
+            response.close();
+          }
+
+        }
+
+
+      } finally {
+        client.close();
       }
 
-
     } finally {
-      client.close();
+      out.close();
     }
-
 
   }
 
